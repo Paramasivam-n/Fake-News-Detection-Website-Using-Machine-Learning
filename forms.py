8570a1bf066c678df8ff34bcfbfff4a807183c15
Paramasivam-n <131851811+Paramasivam-n@users.noreply.github.com>
2023-07-03 20:00:30 +0530

    Codes

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(FlaskForm):
+  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address."), Length(max=255)])
+  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=8, message="Passwords must be at least 8 characters long.")])
+  submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+  password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+  submit = SubmitField("Sign in")
